160f44ab1b0974b2bc9a21bdfb54e98c095af5e8
aurelienCastellarnau <castellarnau.a@gmail.com>
2017-03-29 20:40:17 +0200

    [main] extract database connection instanciate global db object instanciate global claimRequire object (on build) launch routing process

--- api/main.go
+++ api/main.go
@@ -1,36 +1,18 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 
-	"database/sql"
+	"rest/api/routing"
 
-	_ "github.com/go-sql-driver/mysql"
+	"rest/api/dao"
 )
 
-var db *sql.DB
-var claimRequire *sql.DB
-var sqlErr error
-var claimRequireErr error
-
 func main() {
-	db, sqlErr = sql.Open("mysql", "admin:admin@tcp(127.0.0.1:3306)/rest")
-	claimRequire, claimRequireErr = sql.Open("mysql", "admin:admin@tcp(127.0.0.1:3306)/claims")
-	if claimRequireErr != nil {
-		fmt.Printf("\n[claim require] database claims not avalaible. Authentication can't be persisted.")
-	}
-	if sqlErr != nil {
-		fmt.Printf(sqlErr.Error())
-		return
-	}
-	defer db.Close()
-	sqlErr = db.Ping()
-	if sqlErr != nil {
-		fmt.Printf(sqlErr.Error())
-		return
-	}
-	router := NewRouter()
+	dao.GetDb()
+	defer dao.Db.Close()
+	defer dao.ClaimRequire.Close()
+	router := routing.NewRouter()
 	log.Fatal(http.ListenAndServe(":3000", router))
 }
